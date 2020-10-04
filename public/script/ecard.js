a3fc28eba4a9a8d38cd6f0bbcb4ad677a2dabde1
Anish Ghimire <anishghimire862@gmail.com>
2020-10-04 23:12:30 +0545

    Added new feature: Ecard generation

---
+++ public/script/ecard.js
@@ -0,0 +1,44 @@
+
+$('.colors').click(function() {
+	var chosen_color = $(this).css('background-color');
+	$('#canvas').css('background-color', chosen_color);
+});
+
+$('.texture').click(function(){
+	var chosen_texture = $(this).css('background-image');
+	$('#canvas').css('background-image', chosen_texture);
+});
+
+$('#ecardMessage').click(function() {
+  $('#messageOutput').html($(this).val());
+});
+
+$('#recipient').keyup(function() {
+	var recipient = $(this).val();
+	$('#recipientOutput').html(recipient);
+});
+
+$('.stickers').click(function() {
+  var new_sticker = $(this).clone();
+	new_sticker.addClass('stickers_on_card');
+	$('#canvas').prepend(new_sticker);
+	new_sticker.draggable({containment:'#canvas'});
+});
+
+$('#ecardControls').on('click', '.stickers', function () {
+  var new_sticker = $(this).clone();
+  new_sticker.addClass('stickers_on_card');
+  $('#canvas').prepend(new_sticker);
+  new_sticker.draggable({
+      containment: '#canvas',
+      opacity: .35
+  });
+});
+
+$('#refreshEcardButton').click(function() {
+  $('#messageOutput').text('');
+  $('#recipientOutput').text('');
+  $('#canvas').css('background-color', '');
+  $('#canvas').css('background-image', '');
+  $('#canvas img').remove();
+})
