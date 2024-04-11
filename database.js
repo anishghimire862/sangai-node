dfd57c7833b970c370aeebe8629553ad969695bf
Anish Ghimire <anishghimire862@gmail.com>
2024-04-11 15:42:08 +0545

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 const conn = mysql.createConnection({
-    host     : '65.108.59.49',
+    host     : 'localhost',
     port     :  3306,
     user     : 'sangai',
     password : 'sangai',
